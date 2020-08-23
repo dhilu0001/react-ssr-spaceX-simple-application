6c15ccfa62cd63800845d8d5a14c928383608d92
Dhiluraj <dhilu0001@gmail.com>
2020-08-23 19:20:38 +0530

    spaceX code changes

---
+++ src/client/Routes.js
@@ -0,0 +1,20 @@
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+import ArticleListPage from './pages/ArticleListPage';
+import App from './App';
+
+export default [
+  {
+    ...App,
+    routes: [
+      {
+        ...HomePage,
+        path: '/',
+        exact: true,
+      },
+      {
+        ...NotFoundPage,
+      },
+    ],
+  },
+];
